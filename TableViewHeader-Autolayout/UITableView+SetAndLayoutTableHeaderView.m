5a380539e95305eeafda3275be7b8a7b7428a646
Vbon <5914018@qq.com>
2016-12-29 20:46:26 +0800

    使用多叉树遍历view的subViews
    优化功能，使用多叉树的递归遍历。

--- TableViewHeader-Autolayout/UITableView+SetAndLayoutTableHeaderView.m
+++ TableViewHeader-Autolayout/UITableView+SetAndLayoutTableHeaderView.m
@@ -10,15 +10,48 @@
 
 @implementation UITableView (SetAndLayoutTableHeaderView)
 
+/**
+ *
+ *使用AutoLayout来设置TableView的headerView
+ *
+ @param header headerView
+ */
 - (void)setAndLayoutTableHeaderView:(UIView *)header;{
     self.tableHeaderView = header;
     [header setNeedsLayout];
     [header layoutIfNeeded];
+    //刷新布局后进行UILabel的宽度设置。
+    [self parseTreeWithViewSubViewArray:header.subviews];
     CGFloat height = [header systemLayoutSizeFittingSize:UILayoutFittingCompressedSize].height;
     CGRect frame = header.frame;
     frame.size.height = height;
     header.frame = frame;
     self.tableHeaderView = header;
+}
+
+
+/**
+ *
+ *by Vbon
+ *将UIView和subViews组成的结构看成是树，那么这里就形成了多叉树。
+ *使用递归来遍历多叉树。
+ *
+ @param subViewArray subViews的数组
+ */
+- (void)parseTreeWithViewSubViewArray:(NSArray *)subViewArray{
+    if (subViewArray.count == 0) {
+        return;
+    }
+    for (UIView *subView in subViewArray) {
+        if ([subView isKindOfClass:[UILabel class]]) {
+            UILabel *label = (UILabel *)subView;
+            //不设置这个属性会导致算出来的高度不准。
+            label.preferredMaxLayoutWidth = label.frame.size.width;
+        }
+        if (subView.subviews.count > 0) {
+            [self parseTreeWithViewSubViewArray:subView.subviews];
+        }
+    }
 }
 
 @end
